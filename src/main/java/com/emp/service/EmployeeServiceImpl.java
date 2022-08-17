801d699093fec9315d8a3aca3c85a8d26b527a54
unknown <cogjava3104@iiht.tech>
2022-08-17 00:53:59 -0700

    first commit

--- src/main/java/com/emp/service/EmployeeServiceImpl.java
+++ src/main/java/com/emp/service/EmployeeServiceImpl.java
@@ -1,10 +1,12 @@
 package com.emp.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.emp.exception.ResourceNotFoundException;
 import com.emp.model.Employee;
 
 @Service
@@ -23,6 +25,35 @@ public List<Employee> getAllEmployees() {
 	// TODO Auto-generated method stub
 	return employeeRepository.findAll();
 }
+
+@Override
+public Optional<Employee> grtEmployee(Integer id) {
+	
+	return employeeRepository.findById(id);
+}
+
+@Override
+public void deleteEmployee(Integer id) {
+	employeeRepository.deleteById(id);
+}
+
+@Override
+public Employee updateEmployee(Employee employee, Integer id) {
+	//we will check weather the employee exists with the given id or not
+	Employee existingEmployee=employeeRepository.findById(id).orElseThrow(
+			()->new ResourceNotFoundException("Employee", "id", id));
+	existingEmployee.setFirstName(employee.getFirstName());
+	existingEmployee.setLastName(employee.getLastName());
+	existingEmployee.setEmail(employee.getEmail());
+	
+	//we will update the value and then save the updated value
+	employeeRepository.save(existingEmployee);
+	
+	return existingEmployee;
+}
+
+
+
 
 	
 }
--- src/main/java/com/emp/service/IEmpoyeeService.java
+++ src/main/java/com/emp/service/IEmpoyeeService.java
@@ -1,6 +1,7 @@
 package com.emp.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.emp.model.Employee;
 
@@ -9,5 +10,12 @@ public interface IEmpoyeeService {
 	Integer saveEmployee(Employee employee);
 	
 	public List<Employee>getAllEmployees();
+	
+	Optional<Employee>grtEmployee(Integer id);
+	
+	public void deleteEmployee(Integer id);
+	
+	Employee updateEmployee(Employee employee,Integer id);
+	
 	
 }
